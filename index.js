b3f1692423cfa364639545324f32f99b154542b5
Meenaj21 <sourabhmeena0007@gmail.com>
2022-10-10 17:24:05 +0530

    portfolio: change the menu style

--- index.js
+++
@@ -1,6 +0,0 @@
-let ham = document.querySelector('.hamburger');
-let menu = document.querySelector('.sidebar');
-ham.addEventListener('click', (e)=>{
-    ham.classList.toggle('change');
-    menu.classList.toggle('open');
-});
